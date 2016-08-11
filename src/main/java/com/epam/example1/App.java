fbd0359650bd497e7a34b5873f8ae05ca87733f6
Oleksandr_Kokhanyuk <oleksandr_kokhanyuk@epam.com>
2016-08-11 18:15:39 +0300

    first from eclipse

---
+++ src/main/java/com/epam/example1/App.java
@@ -0,0 +1,13 @@
+package com.epam.example1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
